84a0d070bd14541de73cb2474225fd9498c646b2
akiid777 <akiid777@gmail.com>
2022-11-13 21:29:05 +0200

    Add dotenv to the project

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,13 +1,12 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
-
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
+  username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,5 +1,7 @@
-import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
+import { Sequelize } from 'sequelize-typescript';
+import { config } from './config/config';
+
+console.log(config);
 
 export const sequelize = new Sequelize({
   username: config.username,
@@ -7,6 +9,6 @@ export const sequelize = new Sequelize({
   database: config.database,
   host: config.host,
 
-  dialect: "postgres",
-  storage: ":memory:",
+  dialect: 'postgres',
+  storage: ':memory:',
 });
