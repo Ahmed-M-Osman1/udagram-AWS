9e8553016b87b29bb43fab8d2c73ec2f4095c7d8
akiid777 <akiid777@gmail.com>
2022-11-19 21:16:45 +0200

    Fix dependancies and test frontend locally.

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,14 +1,11 @@
 import { Sequelize } from 'sequelize-typescript';
 import { config } from './config/config';
 
-console.log(config);
-
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-
   dialect: 'postgres',
   storage: ':memory:',
 });
