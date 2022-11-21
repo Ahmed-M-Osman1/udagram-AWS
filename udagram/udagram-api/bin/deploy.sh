dc06e4697e59a7252cccbf45ac0efec40cd7d274
akiid777 <akiid777@gmail.com>
2022-11-21 16:07:44 +0200

    Add deploy script

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -4,10 +4,6 @@ set -e
 
 AWS_CONFIG_FILE=$HOME/.aws/config
 
-mkdir $HOME/.aws
-touch $AWS_CONFIG_FILE
-chmod 600 $AWS_CONFIG_FILE
-
 echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
