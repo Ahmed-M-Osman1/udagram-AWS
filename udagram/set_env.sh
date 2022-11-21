09eea7859e066663f9a3d098c5872908535dc851
akiid777 <akiid777@gmail.com>
2022-11-21 07:43:21 +0200

    first upload to test the cloud frontend site

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::ahmedmsoman-udagram
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
+export AWS_PROFILE=AKIAZ4X7P3KFQ4KSWUN7
+export JWT_SECRET=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
 export URL=http://localhost:8100
