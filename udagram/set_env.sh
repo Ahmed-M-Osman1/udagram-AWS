3ab3eee75a75d825a2d353f7f2c590355993c950
akiid777 <akiid777@gmail.com>
2022-11-22 09:42:40 +0200

    Add var

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-2.c2chzapcol5i.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::ahmedmsoman-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=AKIAZ4X7P3KFQ4KSWUN7
 export JWT_SECRET=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
-export URL=http://localhost:8100
+export URL=http://ahmedmsoman-udagram.s3-website-us-east-1.amazonaws.com
