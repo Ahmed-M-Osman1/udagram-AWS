2a18f2a13a1fde63678c4c36bf7c01dcf6fb93bf
akiid777 <akiid777@gmail.com>
2022-11-23 07:35:57 +0200

    ADD EB UR

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::ahmedmsoman-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=AKIAZ4X7P3KFQ4KSWUN7
 export JWT_SECRET=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
-export URL=http://ahmedmosman-udagram-dev.us-east-1.elasticbeanstalk.com/
+export URL=http://ahmedmosman-udagram-dev.us-east-1.elasticbeanstalk.com/api/v0
