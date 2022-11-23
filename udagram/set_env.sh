b490e5a41db06a40249fa4c1d875c650cd2c2127
akiid777 <akiid777@gmail.com>
2022-11-23 15:47:56 +0200

    Add Port

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -9,3 +9,4 @@ export AWS_REGION=us-east-1
 export AWS_PROFILE=AKIAZ4X7P3KFQ4KSWUN7
 export JWT_SECRET=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
 export URL=http://ahmedmosman-udagram-dev.us-east-1.elasticbeanstalk.com/api/v0
+export PORT=5432
