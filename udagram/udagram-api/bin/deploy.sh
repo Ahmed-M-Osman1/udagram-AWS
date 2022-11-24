af4fea4274b7312aa314d45f118145892a8e3d54
akiid777 <akiid777@gmail.com>
2022-11-24 10:39:28 +0200

    add eb

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/env bash
-set -x
-set -e
+eb setenv AWS_BUCKET=$AWS_BUCKET AWS_PROFILE=$AWS_PROFILE AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  JWT_SECRET=$JWT_SECRET PORT=$PORT DB_PORT=$DB_PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL
 
-AWS_CONFIG_FILE=$HOME/.aws/config
-
-echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
-
-cp -R .elasticbeanstalk www/.elasticbeanstalk
-cp .npmrc www/.npmrc 
-cp package.json www/package.json 
-cd www && zip -r Archive.zip . 
-cd ..
+eb deploy
