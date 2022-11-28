8bd5be703ea49174fc091504d913e37bce4750ee
akiid777 <akiid777@gmail.com>
2022-11-28 13:07:13 +0200

    Fix ports

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-2.c2chzapcol5i.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.c2chzapcol5i.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::ahmedmsoman-udagram
 export AWS_REGION=us-east-1
