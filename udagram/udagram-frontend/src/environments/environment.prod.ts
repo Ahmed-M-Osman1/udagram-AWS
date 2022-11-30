5ce03c2b168cc9ba067a41dca8f630ed302596c7
akiid777 <akiid777@gmail.com>
2022-12-01 00:53:38 +0200

    try a new way

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -8,7 +8,6 @@ export const environment = {
   apiHost:
     'http://ahmedmosman-udagram-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
