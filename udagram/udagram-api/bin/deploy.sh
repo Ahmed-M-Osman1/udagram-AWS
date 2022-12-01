898a13813783d03160f9f194418e98b3e63ddfc3
akiid777 <akiid777@gmail.com>
2022-12-01 14:07:32 +0200

    API RESET

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 AWS_CONFIG_FILE=$HOME/.aws/config
-
+eb init
 echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
