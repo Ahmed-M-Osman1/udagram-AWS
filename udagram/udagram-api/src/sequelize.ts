2ec811aa22db0c2db762aa2431246f0b639e5b53
Ahmed <M Osman>
2022-12-01 16:06:45 +0200

    Add new var

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -8,7 +8,7 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
-  port: config.port,
+  port: config.dbPort,
   dialect: 'postgres',
   storage: ':memory:',
 });
